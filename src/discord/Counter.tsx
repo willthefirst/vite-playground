a26afa6505b0cf7349fb46c50be7570d916894dd
Will Moritz <willthefirst@gmail.com>
2023-11-29 17:19:14 -0800

    Added tailwind

--- src/discord/Counter.tsx
+++ src/discord/Counter.tsx
@@ -6,14 +6,16 @@ export function Counter() {
   const dispatch = useAppDispatch()
 
   return (
-    <div>
+    <div className='border-blue-600 border-2 p-6'>
       <button
+        className='bg-blue-100'
         aria-label='Increment value'
         onClick={() => dispatch(increment())}>
         Increment
       </button>
       <span>{count}</span>
       <button
+        className='bg-red-400'
         aria-label='Decrement value'
         onClick={() => dispatch(decrement())}>
         Decrement
